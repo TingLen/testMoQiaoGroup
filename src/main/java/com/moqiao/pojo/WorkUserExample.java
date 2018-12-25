6a39986f075de7624e112f6dfb09fb0f795190d2
Linyan1211 <peng.dl@outlook.com>
2018-12-25 15:21:26 +0800

    唐更新xxxxx

--- src/main/java/com/moqiao/pojo/WorkUserExample.java
+++ src/main/java/com/moqiao/pojo/WorkUserExample.java
@@ -1,10 +1,7 @@
 package com.moqiao.pojo;
 
-import org.springframework.context.annotation.Bean;
-
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class WorkUserExample {
     protected String orderByClause;
