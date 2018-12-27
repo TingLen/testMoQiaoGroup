43e07a31bfd76ec2cf6c7d379aa650673c0d1858
PengDingLin <peng.dl@outlook.com>
2018-12-27 16:58:42 +0800

    完成user login功能及单元测试

--- src/main/java/com/moqiao/controller/UserController.java
+++ src/main/java/com/moqiao/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -26,83 +27,22 @@ public class UserController {
     @ApiImplicitParam(name = "workUser", value = "用户登录前端传来的form workUser",dataType = "WorkUser",required = true)
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public LoginMessage login(@RequestBody WorkUser workUser ){
-//        LoginMessage loginMessage = new LoginMessage();
-//
-//        WorkUser resWorkUser = userService.login(workUser.getUsercode(),workUser.getUserpassword());
-//
-//        //若返回对象不为null,则说明数据库有该账号数据
-//        if(resWorkUser != null){
-//            try {
-//                //前端传入的密码md5加密
-//                byte[] bytesOfPassword = workUser.getUserpassword().getBytes("UTF-8");
-//                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-//                byte[] passwordMd5 = messageDigest.digest(bytesOfPassword);
-//
-//                //与后端返回值对比
-//
-//                new String(passwordMd5,"UTF-8");
-//
-//                if(resWorkUser.getUserpassword().equals(new String(passwordMd5,"UTF-8"))){
-//                    loginMessage.setSuccess(true);
-//                    loginMessage.setMessage("登陆成功");
-//                    return loginMessage;
-//                }
-//                loginMessage.setSuccess(false);
-//                loginMessage.setMessage("密码错误");
-//                return loginMessage;
-//
-//
-//            } catch (NoSuchAlgorithmException e) {
-//                e.printStackTrace();
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        loginMessage.setMessage("账号错误");
-//        loginMessage.setSuccess(false);
-//        return loginMessage;
-        /**
-         * 1.将前端密码进行md5加密
-         * 2.调用service.login
-         * 3.判断用户是否存在
-         */
-
+        LoginMessage loginMessage = new LoginMessage();
 
         //前端传入的密码md5加密
-        try {
-            MessageDigest messageDigest = null;
-            messageDigest = MessageDigest.getInstance("MD5");
-            byte[] passwordMd5 = messageDigest.digest(workUser.getUserpassword().getBytes("UTF-8"));
 
-            //调用service
-            WorkUser resWorkUser = userService.login(workUser.getUsercode(),new String(passwordMd5));
-
-            //若返回的resWorkUser不为null，则表示账号密码正确在数据库中找到用户
-            if(resWorkUser != null){
-
-                loginMessage.setSuccess(true);
-                loginMessage.setMessage("登陆成功");
-                return loginMessage;
-            }
-                loginMessage.setSuccess(false);
-                loginMessage.setMessage("账号或密码错误");
-                return loginMessage;
-
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        //调用service
+        int i  = userService.login(workUser.getUsercode(),DigestUtils.md5DigestAsHex(workUser.getUserpassword().getBytes()));
+        if(i != 0){
+            loginMessage.setSuccess(true);
+            loginMessage.setMessage("登陆成功");
+            return loginMessage;
         }
 
         loginMessage.setSuccess(false);
-        loginMessage.setMessage("网络错误");
+        loginMessage.setMessage("账号或密码错误");
         return loginMessage;
-
-
-
-
-
-
     }
+
+
 }
--- src/test/java/com/moqiao/UserTest.java
+++ src/test/java/com/moqiao/UserTest.java
@@ -1,17 +1,28 @@
 package com.moqiao;
 
 import com.moqiao.controller.UserController;
+import com.moqiao.pojo.WorkUser;
+import com.moqiao.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.nio.charset.Charset;
+
+import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -20,19 +31,26 @@ public class UserTest {
 
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserController userController;
+
     @Before
     public void setMockMvc(){
-        mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
     }
 
     @Test
     public void loginSuccessTest() throws Exception {
+
+        String json = "{ \"titlecode\": \"string\", \"usercode\": \"lmh111\", \"userpassword\": \"lmh111\"}";
         RequestBuilder request = null;
         request = post("/user/login")
-                .param("usercode","lmh111")
-                .param("userpassword","lmh111");
+                .contentType("application/json")
+                .content(json);
 
         mockMvc.perform(request)
-                .andExpect(status().isOk());
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("{\"success\":true,\"message\":\"登陆成功\"}"));
     }
 }
