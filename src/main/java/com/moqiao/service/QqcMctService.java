b3d1b3e9727be4c391cd1d712af4ea1ec9f03338
yu <zhibin_yu@163.com>
2018-12-27 10:57:16 +0800

    yuxxxx

---
+++ src/main/java/com/moqiao/service/QqcMctService.java
@@ -0,0 +1,28 @@
+package com.moqiao.service;
+
+import com.moqiao.dao.mapper.WorkUserMapper;
+import com.moqiao.pojo.WorkUser;
+import com.moqiao.pojo.WorkUserExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QqcMctService {
+    @Autowired
+    private WorkUserMapper workUserMapper;
+
+    private WorkUserExample example = new WorkUserExample();
+    @Autowired
+    private WorkUser workUser;
+
+    public WorkUser login(String usercode, String userpassword){
+        example.or().andUsercodeEqualTo(usercode).andUserpasswordEqualTo(userpassword);
+        List<WorkUser> workUserList = workUserMapper.selectByExample(example);
+        if(workUserList.size() > 0){
+            return workUserList.get(0);
+        }
+        return null;
+    }
+}
