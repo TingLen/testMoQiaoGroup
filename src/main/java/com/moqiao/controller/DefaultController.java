3976c2d07197704c6127a604c4b0cad0542eb289
yu <zhibin_yu@163.com>
2018-12-27 11:02:14 +0800

    yuxxxx

--- src/main/java/com/moqiao/controller/DefaultController.java
+++ src/main/java/com/moqiao/controller/DefaultController.java
@@ -1,10 +1,16 @@
 package com.moqiao.controller;
 
+import com.moqiao.message.LoginMessage;
+import com.moqiao.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class DefaultController {
+    @Autowired
+    private UserService userService;
 
+    private LoginMessage loginMessage;
 }
